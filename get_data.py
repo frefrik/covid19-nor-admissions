23f2e750853f7531fef06ffe2e5e8743e5d157c0
frefrik <fredrik@haarstad.no>
2020-05-06 13:31:12 +0200

    Restructure script

--- get_data.py
+++ get_data.py
@@ -1,7 +1,7 @@
 import requests
 import yaml
 import pandas as pd
-from pandas.io.json import json_normalize
+import traceback
 
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -12,33 +12,41 @@ headers = {
     'Ocp-Apim-Subscription-Key': cfg['api']['key'],
 }
 
-def health_nat():
+def write_df(df):
+    filename = traceback.extract_stack(None, 2)[0][2]
+    df.to_csv('data/' + filename + '.csv', encoding='utf-8', index=False)
+    df.to_excel('data/' + filename + '.xlsx', encoding='utf-8', index=False)
+
+def admissions_nat():
     res = requests.get(url + 'nasjonalt', headers=headers).json()
     df = pd.DataFrame(res['registreringer'])
     df = df.rename(columns={'dato': 'date', 'antInnlagte': 'admissions', 'antRespirator': 'respiratory'})
+    df = df[['date', 'admissions', 'respiratory']]
     df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
     df = df.sort_values(['date'], ascending=True)
-    df.to_csv('data/admissions_nat.csv', encoding='utf-8', index=False)
-    df.to_excel('data/admissions_nat.xlsx', encoding='utf-8', index=False)
 
-def health_reg():
+    write_df(df)
+
+def admissions_reg():
     res = requests.get(url + 'helseregion', headers=headers).json()
-    df = json_normalize(res, 'registreringer', ['navn'])
+    df = pd.json_normalize(res, 'registreringer', ['navn'])
     df = df.rename(columns={'dato': 'date', 'navn': 'health_reg_name', 'antInnlagte': 'admissions', 'antRespirator': 'respiratory'})
+    df = df[['date', 'health_reg_name', 'admissions', 'respiratory']]
     df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
-    df.to_csv('data/admissions_reg.csv', columns=['date', 'health_reg_name', 'admissions', 'respiratory'], encoding='utf-8', index=False)
-    df.to_excel('data/admissions_reg.xlsx', columns=['date', 'health_reg_name', 'admissions', 'respiratory'], encoding='utf-8', index=False)
 
-def health_org():
+    write_df(df)
+
+def admissions_org():
     res = requests.get(url + 'helseforetak', headers=headers).json()
-    df = json_normalize(res, 'covidRegistreringer', ['helseforetakNavn', 'region'])
+    df = pd.json_normalize(res, 'covidRegistreringer', ['helseforetakNavn', 'region'])
     df = df.rename(columns={'dato': 'date', 'helseforetakNavn': 'health_org_name', 'region': 'health_reg_name', 'antInnlagte': 'admissions', 'antRespirator': 'respiratory'})
+    df = df[['date', 'health_reg_name', 'health_org_name', 'admissions']]
     df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
     df = df.sort_values(['health_reg_name', 'health_org_name', 'date'], ascending=True)
-    df.to_csv('data/admissions_org.csv', columns=['date', 'health_reg_name', 'health_org_name', 'admissions'], encoding='utf-8', index=False)
-    df.to_excel('data/admissions_org.xlsx', columns=['date', 'health_reg_name', 'health_org_name', 'admissions'], encoding='utf-8', index=False)
+
+    write_df(df)
 
 if __name__ == '__main__':
-    health_nat()
-    health_reg()
-    health_org()
+    admissions_nat()
+    admissions_reg()
+    admissions_org()
